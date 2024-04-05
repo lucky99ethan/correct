545fa3110cf4643383bd8cb635e48e8340e1d820
lucky99ethan <ethansawe99@gmail.com>
2024-04-05 20:03:45 +0300

    change one

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "correct": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
